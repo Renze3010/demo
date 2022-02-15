61e867d65d45cabd19e24c687e6f9925a16a8371
Renze3010 <rjboerman@ziggo.nl>
2022-02-15 12:39:18 +0100

    initial commit

---
+++ demo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-*=6^6nc%_6kr($wbrik$43x2ti(m$&($uplzsh6soe+!_7=v7d'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
